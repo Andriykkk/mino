dd2764fbd66ccbb89877076bc58ca6c676b73775
andriykkk <andriykostenkostd@gmail.com>
2025-02-23 21:37:53 +0200

    add relu

--- src/mino/activations/relu.lua
+++ src/mino/activations/relu.lua
@@ -1,29 +1,34 @@
-local relu = {}
 local matrix = require('matrix')
+local relu = {}
 
 function relu.relu(input)
-    local result = matrix.new({dims = {input.dims[1], input.dims[2]}})
-    for i = 1, #input.data do
+    local result = input:copy({data = 0})
+
+    for i = 1, input.size do
         if input.data[i] < 0 then
             result.data[i] = 0
         else
             result.data[i] = input.data[i]
         end
-    end 
+    end
+
     result.backward = relu.relu_backward
     result.operand1 = input
+
     return result
 end
 
 function relu.relu_backward(self, respect)
-    for i = 1, #respect.data do
+    for i = 1, respect.size do
         if respect.data[i] < 0 then
             respect.data[i] = 0
         end
     end
 
-    for i = 1, self.dims[1] * self.dims[2] do
-        self.operand1.grad[i] = self.operand1.grad[i] + respect.data[i]
+    if self.operand1.required_grad == true then
+        for i = 1, respect.size do
+            self.operand1.grad[i] = self.operand1.grad[i] + respect.data[i]
+        end
     end
 
     if self.operand1.backward then
--- src/mino/mino.lua
+++ src/mino/mino.lua
@@ -1,7 +1,14 @@
+package.path = package.path .. ";./mino/activations/?.lua;"
+
 local matrix = require('matrix')
 local mino = {}
 
+local activations = {
+    relu = require('relu'),
+}
+
 mino.Matrix = matrix
+mino.activations = activations
 
 
 
